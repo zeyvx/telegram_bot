9c1bcc42491a5e81f11bd4038673716c7914cba6
xasanov <xasanovbekzod6261@gmail.com>
2026-08-16 19:45:08 +0500

    feat: export community and member repositories

--- packages/database/src/repositories/index.ts
+++ packages/database/src/repositories/index.ts
@@ -2,3 +2,5 @@ export * from "./authorization.js";
 export * from "./roles.js";
 export * from "./audit.js";
 export * from "./community-access.js";
+export * from "./members.js";
+export * from "./users.js";
